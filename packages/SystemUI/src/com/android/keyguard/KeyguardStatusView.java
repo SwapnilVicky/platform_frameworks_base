b429ddb21bb612deff8d0587ebd6a7e10a4c7014
Joe Maples <joe@frap129.org>
2018-01-16 12:18:02 -0500

    keyguard: Actually kill Fancy Colon
    https://github.com/CarbonROM/android_frameworks_base/commit/b670f4b2d1e66870b7d5b61d4f6cb371c7f5dbb0 claims to have done this, but it didnt
    
    Change-Id: I4eefdf9dd8f1f48d793a371189d471a5282ea745
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -84,9 +84,6 @@ public class KeyguardStatusView extends GridLayout implements
         mStatusViewContainer = findViewById(R.id.status_view_container);
 
         mClockView = findViewById(R.id.keyguard_clock_container);
-        if (KeyguardClockAccessibilityDelegate.isNeeded(mContext)) {
-            mClockView.setAccessibilityDelegate(new KeyguardClockAccessibilityDelegate(mContext));
-        }
 
         mKeyguardSlice = findViewById(R.id.keyguard_slice_view);
 
